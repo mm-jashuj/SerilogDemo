6b0f26127bdabf1c4feb1abbb8d7fb73c1c1e915
MAGNUSMINDS073\DELL <mm.jashuj@gmail.com>
2024-12-25 19:00:23 +0530

    Add project files.

---
+++ SerilogDemo/Program.cs
@@ -0,0 +1,39 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+var logger = new LoggerConfiguration()
+  .ReadFrom.Configuration(builder.Configuration)
+  .Enrich.FromLogContext()
+  .WriteTo.Seq(builder.Configuration["Seq:ServerURL"])
+  .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.Services.AddSerilog(logger);
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
